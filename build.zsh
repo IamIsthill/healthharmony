fdf9c55770018487651be778b63cff5fd7823c4b
IamIsthill <159409061+IamIsthill@users.noreply.github.com>
2024-05-08 19:28:21 +0800

    First Commit

---
+++ build.zsh
@@ -0,0 +1,22 @@
+source .venv/bin/activate
+
+function run(){
+  python manage.py runserver
+}
+
+function save(){
+  pip freeze > requirements.txt
+}
+
+function app(){
+  python manage.py startapp "$1"
+}
+
+function migrate(){
+  python manage.py makemigrations
+  python manage.py migrate
+}
+
+function build(){
+  pip install -r requirements.txt
+}
