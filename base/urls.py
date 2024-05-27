36a02855a153a7cc8e8994cc3165aabad1d11860
IamIsthill <159409061+IamIsthill@users.noreply.github.com>
2024-05-27 13:09:19 +0800

    Added admin app

--- base/urls.py
+++ base/urls.py
@@ -2,7 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    # path("", include('base.urls'))
     path("", views.home, name="home"),
     path("login/", views.login, name="login"),
     path("weather/", views.weather, name="weather"),
