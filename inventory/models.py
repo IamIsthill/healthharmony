56603f83374808ee07a5657eb6d4a4c5c862a54a
IamIsthill <159409061+IamIsthill@users.noreply.github.com>
2024-06-20 22:22:11 +0800

    Working on something

--- inventory/models.py
+++ inventory/models.py
@@ -7,15 +7,16 @@ class InventoryDetail(models.Model):
     item_name = models.CharField(max_length=255, null=False)
     description = models.TextField(null=False)
     expiration_date= models.DateField(default=None)
+    orig_quantity = models.SmallIntegerField(default=None)
 
     def __str__(self):
         return self.item_name
     
 class QuantityHistory(models.Model):
     inventory = models.ForeignKey(InventoryDetail, on_delete=models.SET_NULL, null=True)
-    less = models.PositiveIntegerField(null=False)
+    updated_quantity = models.PositiveIntegerField(null=False)
     timestamp = models.DateTimeField(auto_created=True)
 
     def __str__(self):
-        view = f'{self.inventory.item_name} - {self.less}'
+        view = f'{self.inventory.item_name} - {self.updated_quantity}'
         return view
