4eea1ef2d2d3539fabf80300742dc168bd778b79
IamIsthill <159409061+IamIsthill@users.noreply.github.com>
2024-06-26 00:16:43 +0800

    Added blood pressure

--- users/models.py
+++ users/models.py
@@ -7,8 +7,19 @@ class User(AbstractUser):
     first_name = models.CharField(max_length=200, null=True)
     last_name = models.CharField(max_length=200, null=True)
     email = models.EmailField(null=True, unique=True)
+    access = models.CharField(max_length=100,default='patient')
+    DOB = models.DateField(null=True)
+    sex = models.CharField(max_length=20, null=True)
+    blood_type = models.CharField(max_length=20, null=True)
+    height = models.SmallIntegerField(null=True)
+    weight = models.SmallIntegerField(null=True)
+    username = None
 
     USERNAME_FIELD='email'
-    REQUIRED_FIELDS=[]
+    REQUIRED_FIELDS=['first_name', 'last_name']
+    
 
     objects = UserManager()
+
+    def __str__(self):
+        return self.email
