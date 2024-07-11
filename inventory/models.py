87e4a5d1bb870f610c3e40c0baf1519d441eac8e
IamIsthill <159409061+IamIsthill@users.noreply.github.com>
2024-07-11 10:22:13 +0800

    Fixed the rendering of inventory data

--- inventory/models.py
+++ inventory/models.py
@@ -2,20 +2,24 @@ from django.db import models
 
 # Create your models here.
 class InventoryDetail(models.Model):
+    CATEGORY_CHOICES = [
+        ('medicine', 'Medicine'),
+        ('supply', 'Supply'),
+    ]
     item_no = models.SmallIntegerField(default=None)
-    unit = models.CharField(max_length=100, null=True)
-    item_name = models.CharField(max_length=255, null=False)
+    unit = models.CharField(max_length=50, null=True)
+    item_name = models.CharField(max_length=100, null=False)
+    category = models.CharField(max_length=10, choices=CATEGORY_CHOICES, default='Medicine')
     description = models.TextField(null=False)
     expiration_date= models.DateField(default=None)
-    orig_quantity = models.SmallIntegerField(default=None)
 
     def __str__(self):
         return self.item_name
     
 class QuantityHistory(models.Model):
     inventory = models.ForeignKey(InventoryDetail, on_delete=models.SET_NULL, null=True)
-    updated_quantity = models.PositiveIntegerField(null=False)
-    timestamp = models.DateTimeField(auto_created=True)
+    updated_quantity = models.IntegerField(null=False, blank=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         view = f'{self.inventory.item_name} - {self.updated_quantity}'
