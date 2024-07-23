98aa7f488c952d357fab1ad78dc4b408e1ec19cb
IamIsthill <159409061+IamIsthill@users.noreply.github.com>
2024-07-23 14:47:55 +0800

    created the patient, added the debug toolbar for caching

--- base/urls.py
+++ base/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("", include('users.urls')),
     path("staff/", include('staff.urls')),
+    path("doctor/", include('doctor.urls')),
+    path("patient/", include('patient.urls')),
     path("api/",include('api.urls')),
     path("administrator/", include('administrator.urls')),
     path("accounts/", include("allauth.urls")),
