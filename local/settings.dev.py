cd9813af5679ac6dc07fded32f9772865e75fdc4
IamIsthill <159409061+IamIsthill@users.noreply.github.com>
2024-07-31 14:16:38 +0800

    Added the colorlog

--- local/settings.dev.py
+++ local/settings.dev.py
@@ -1,3 +1,6 @@
+from healthharmony.app.settings.logging import LOGGING
+
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-s(5zc6(qerhzjavz+s-2t=2e*sud4*)8_5g74b#dw(um4%np2+"
 
@@ -5,3 +8,12 @@ SECRET_KEY = "django-insecure-s(5zc6(qerhzjavz+s-2t=2e*sud4*)8_5g74b#dw(um4%np2+
 DEBUG = True
 
 ALLOWED_HOSTS = []
+
+LOGGING["formatters"]["colored"] = {  # type: ignore noqa: F821
+    "()": "colorlog.ColoredFormatter",
+    "format": "%(log_color)s%(asctime)s %(levelname)s %(name)s %(bold_white)s%(message)s",
+}
+
+LOGGING["loggers"]["healthharmony"]["level"] = "DEBUG"  # type: ignore
+LOGGING["handlers"]["console"]["level"] = "DEBUG"  # type: ignore
+LOGGING["handlers"]["console"]["formatter"] = "colored"  # type: ignore
