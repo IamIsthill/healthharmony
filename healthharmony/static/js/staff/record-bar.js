9639cd1fda0038214fdbfe5690e88e5389a72136
IamIsthill <159409061+IamIsthill@users.noreply.github.com>
2024-09-22 18:16:59 +0800

    Fixed bug in records

--- healthharmony/static/js/staff/record-bar.js
+++ healthharmony/static/js/staff/record-bar.js
@@ -7,25 +7,25 @@ export function createRequestBarChart(labels, counts, createChart) {
         datasets: [{
             data: counts,
             backgroundColor: [
-              'rgba(255, 99, 132, 0.2)',
-              'rgba(255, 159, 64, 0.2)',
-              'rgba(255, 205, 86, 0.2)',
-              'rgba(75, 192, 192, 0.2)',
-              'rgba(54, 162, 235, 0.2)',
-              'rgba(153, 102, 255, 0.2)',
-              'rgba(201, 203, 207, 0.2)'
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+                'rgba(255, 205, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(201, 203, 207, 0.2)'
             ],
             borderColor: [
-              'rgb(255, 99, 132)',
-              'rgb(255, 159, 64)',
-              'rgb(255, 205, 86)',
-              'rgb(75, 192, 192)',
-              'rgb(54, 162, 235)',
-              'rgb(153, 102, 255)',
-              'rgb(201, 203, 207)'
+                'rgb(255, 99, 132)',
+                'rgb(255, 159, 64)',
+                'rgb(255, 205, 86)',
+                'rgb(75, 192, 192)',
+                'rgb(54, 162, 235)',
+                'rgb(153, 102, 255)',
+                'rgb(201, 203, 207)'
             ],
             borderWidth: 1
-          }]
+        }]
     }
     const chartOptions = {
         plugins: {
@@ -71,10 +71,12 @@ export function getCountsAndLabelForRequestBar(data) {
     let counts = []
     let labels = []
 
-    for (const item of data) {
-        for (const [key, value] of Object.entries(item)) {
-            labels.push(key)
-            counts.push(value)
+    if (data) {
+        for (const item of data) {
+            for (const [key, value] of Object.entries(item)) {
+                labels.push(key)
+                counts.push(value)
+            }
         }
     }
 
