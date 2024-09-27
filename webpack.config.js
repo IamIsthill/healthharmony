a0d3d10102167bd5e0907223b6e066b217c9b2da
IamIsthill <159409061+IamIsthill@users.noreply.github.com>
2024-09-27 08:31:47 +0800

    Config for webpack, preparation for illness categories

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+    entry: {
+        doctor_overview: './healthharmony/static/js/doctor/overview.js',
+        doctor_patient: './healthharmony/static/js/doctor/patient.js',
+        doctor_handled: './healthharmony/static/js/doctor/handled.js',
+        staff_account: './healthharmony/static/js/staff/account.js',
+        staff_inventory: './healthharmony/static/js/staff/inventory.js',
+        staff_overview: './healthharmony/static/js/staff/overview.js',
+        staff_records: './healthharmony/static/js/staff/records.js',
+        patient_overview: './healthharmony/static/js/patient/overview.js',
+        patient_profile: './healthharmony/static/js/patient/profile.js',
+        patient_record: './healthharmony/static/js/patient/record.js',
+        admin_records: './healthharmony/static/js/administrator/records.js',
+        admin_account: './healthharmony/static/js/administrator/account.js',
+    },
+    output: {
+        path: path.resolve(__dirname, './healthharmony/static/js/dist')
+    }
+};
