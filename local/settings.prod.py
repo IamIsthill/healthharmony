d92362a15f70e91a0610f5c74af40185c6faa865
IamIsthill <159409061+IamIsthill@users.noreply.github.com>
2024-10-10 20:59:17 +0800

    Changed ip

--- local/settings.prod.py
+++ local/settings.prod.py
@@ -10,7 +10,7 @@ ALLOWED_HOSTS = [
     "healthharmony.duckdns.org",
     "localhost",
     "127.0.0.1",
-    "34.105.90.154",  # Change the IP everytime
+    "35.185.245.154",  # Change the IP everytime
 ]
 
 if not DEBUG:
