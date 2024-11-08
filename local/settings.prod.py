fa41c20178b034e0a24d5b242478f1322c481fbc
IamIsthill <159409061+IamIsthill@users.noreply.github.com>
2024-11-08 19:39:47 +0800

    change db

--- local/settings.prod.py
+++ local/settings.prod.py
@@ -35,12 +35,13 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "postgres",
-        # "HOST": env("DATABASE_HOST"),
-        "HOST": "35.240.250.37",
+        "HOST": env("DATABASE_HOST"),
+        # "HOST": "35.240.250.37",
         "PORT": 5432,
-        # "USER": env("DATABASE_USER"),
-        "USER": "healthharmony",
-        "PASSWORD": env("GOOGLE_DB_PASSWORD"),
+        "USER": env("DATABASE_USER"),
+        # "USER": "healthharmony",
+        # "PASSWORD": env("GOOGLE_DB_PASSWORD"),
+        "PASSWORD": env("DATABASE_PASSWORD"),
     }
 }
 
